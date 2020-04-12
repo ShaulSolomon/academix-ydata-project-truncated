63fc5c34375a6ec09a61868c97c73d363c97122a
Roy Granit <roy@academix.bio>
2020-04-12 21:26:39 +0300

    prettify the s3 functions file

--- code/s3_functions.py
+++ code/s3_functions.py
@@ -3,7 +3,10 @@
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 from boto import s3
-import boto3, os, re, ssl
+import boto3
+import os
+import re
+import ssl
 import pandas as pd
 import numpy as np
 from configparser import ConfigParser
@@ -13,15 +16,17 @@ creds_path = "credentials.ini"
 config_parser = ConfigParser()
 config_parser.read(creds_path)
 s3_dict = config_parser["S3"]
-AWS_ACCESS_KEY=s3_dict['AWS_ACCESS_KEY']
-AWS_ACCESS_SECRET_KEY=s3_dict['AWS_ACCESS_SECRET_KEY']
-BUCKET=s3_dict['BUCKET']
+AWS_ACCESS_KEY = s3_dict['AWS_ACCESS_KEY']
+AWS_ACCESS_SECRET_KEY = s3_dict['AWS_ACCESS_SECRET_KEY']
+BUCKET = s3_dict['BUCKET']
 #####
+
 
 def get_creds():
         return s3_dict
 
-def list_files_in_bucket(AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY, bucket):
+
+def list_files_in_bucket(AWS_ACCESS_KEY=AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY=AWS_ACCESS_SECRET_KEY, bucket=BUCKET):
         """
         returns a list of file in a bucket
         """
@@ -30,7 +35,8 @@ def list_files_in_bucket(AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY, bucket):
         mybucket = conn.get_bucket(bucket)
         return [i for i in mybucket.list()]
 
-def get_dataframe_from_s3(AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY, bucket, file="data.csv"):
+
+def get_dataframe_from_s3(AWS_ACCESS_KEY=AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY=AWS_ACCESS_SECRET_KEY, bucket=BUCKET, file="data.csv"):
   conn = S3Connection(AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY)
   conn.auth_region_name = 'eu-west-1.amazonaws.com'
   mybucket = conn.get_bucket(bucket)
@@ -41,7 +47,8 @@ def get_dataframe_from_s3(AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY, bucket, file="d
   df = pd.read_csv(file)
   return df
 
-def upload_to_s3(aws_access_key_id, aws_secret_access_key, file, bucket, key, callback=None, md5=None, reduced_redundancy=False, content_type=None):
+
+def upload_to_s3(file, key, AWS_ACCESS_KEY=AWS_ACCESS_KEY, AWS_ACCESS_SECRET_KEY=AWS_ACCESS_SECRET_KEY, bucket=BUCKET,  callback=None, md5=None, reduced_redundancy=False, content_type=None):
     """
     Uploads the given file to the AWS S3
     bucket and key specified.
@@ -65,24 +72,26 @@ def upload_to_s3(aws_access_key_id, aws_secret_access_key, file, bucket, key, ca
         # so we fall back on this
         file.seek(0, os.SEEK_END)
         size = file.tell()
-    
+
     conn = boto.connect_s3(aws_access_key_id, aws_secret_access_key)
     bucket = conn.get_bucket(bucket, validate=True)
     k = Key(bucket)
     k.key = key
     if content_type:
         k.set_metadata('Content-Type', content_type)
-    sent = k.set_contents_from_file(file, cb=callback, md5=md5, reduced_redundancy=reduced_redundancy, rewind=True)
-    
+    sent = k.set_contents_from_file(
+        file, cb=callback, md5=md5, reduced_redundancy=reduced_redundancy, rewind=True)
+
     # Rewind for later use
     file.seek(0)
-    
+
     if sent == size:
         return True
     return False
 
 
-#code used to solve some issue with ssl in mac
-if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-getattr(ssl, '_create_unverified_context', None)):
-    ssl._create_default_https_context = ssl._create_unverified_context
+def solve_mac_issue():
+        #code used to solve some issue with ssl in mac
+        if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+                getattr(ssl, '_create_unverified_context', None)):
+                ssl._create_default_https_context = ssl._create_unverified_context
