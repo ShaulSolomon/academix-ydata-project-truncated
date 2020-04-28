f62461c014055af1bbbe30053b39555faba40f52
Shaul Solomon <Shaulsolomon@gmail.com>
2020-04-28 18:41:50 +0300

    Finished basic LR with hyperparameterization

--- code/lr_model_3.py
+++ code/lr_model_3.py
@@ -9,8 +9,10 @@ if os.getcwd() != CWD:
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 from boto import s3
-#import boto3
+import boto3
 import importlib
+import math
+from sklearn.model_selection import GridSearchCV
 
 
 import pandas as pd
@@ -82,29 +84,9 @@ def get_similarity_matrix(ps,authors_dfs):
   sim_matrix['same_author'] = pair_col
 
   ### --- Removing Pairs --- ###
-
-  # If we have N documents, the matrix will give us an NxN similarity matrix. 
-  # We can simplify our task by removing N_i x N_i (a document with itself)
-  # And by removing N_j x N_i (where j > i), because it will already be covered by N_i x N_j 
   
   print("Removing Doubles")
-  #Get all possible pairs of combination expressed in "AA", "AB", etc.
-  # pairs = []
-  # for i in range(num_papers):
-  #   outer_index = chr(65+i)
-  #   for j in range(num_papers):
-  #     inner_index = chr(65 + j)
-  #     pairs.append(outer_index + inner_index)
 
-  # #join it back to sim_matrix
-  # sim_matrix['pairs'] = [pair for pair in pairs]
-
-  # split_pairs = [list(pair) for pair in pairs]
-  # #keep instance only where N_i x N_j (where i < j, which includes i == j and i < j)
-  # split_pairs = ["".join(pair) for pair in split_pairs if ord(pair[0]) < ord(pair[1])]
-  # #filter matrix
-  # sim_matrix = sim_matrix.loc[sim_matrix['pairs'].isin(split_pairs)]
-  # sim_matrix.set_index("pairs",inplace=True)
   pairs = []
   for i in range(num_papers):
     for j in range(num_papers):
@@ -116,6 +98,7 @@ def get_similarity_matrix(ps,authors_dfs):
   sim_matrix = sim_matrix.iloc[pairs]
 
   print("Returning Similarity Matrix.")
+  print("Number of pairs after cleaning: ", len(sim_matrix.index))
   return sim_matrix
 
 def get_train_test(df,perc_change = 0.8):
@@ -134,13 +117,33 @@ def get_train_test(df,perc_change = 0.8):
   test_df = pd.concat((df_same[perc_train:],df_dif[perc_train:]))
   return train_df.iloc[:,:-1] , train_df.iloc[:,-1], test_df.iloc[:,:-1], test_df.iloc[:,-1]
 
+
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+
 def log_model(X_train,y_train,X_test,y_test):
-  lr_model = LogR(penalty='l2',max_iter=1000,random_state=42)
-  lr_model.fit(X_train,y_train)
-  #train LR with 0- same author 1- dif author
-  y_hat = lr_model.predict_proba(X_test)
-  pred_prob = [max(vals) for vals in y_hat]
-  return pred_prob
+  '''
+  input: X_train, y_train, X_test, y_test
+  return: Score, predict_prob
+  '''
+  penalty = ['l1', 'l2']
+  # Create regularization hyperparameter space
+  C = np.logspace(0, 4, 10)
+  # Create hyperparameter options
+  hyperparameters = dict(C=C, penalty=penalty)
+  log = LogR()
+  clf = GridSearchCV(log, hyperparameters, cv=5, verbose=0)
+  best_model = clf.fit(X_train, y_train)
+  best_penalty = best_model.best_estimator_.get_params()['penalty']
+  best_C = best_model.best_estimator_.get_params()['C']
+  print('Best Penalty:', best_penalty)
+  print('Best C:', best_C)
+  weights = best_model.best_estimator_.coef_.flatten()
+  bias = best_model.best_estimator_.intercept_.flatten()
+  print("Calculating predict probability")
+  predict_prob = [sigmoid(np.dot(x_test,weights) + bias[0]) for x_test in X_test.to_numpy()]
+  score = clf.score(X_test,y_test)
+  return score, predict_prob
 
 if __name__ == "__main__":
   CWD = 'c:\\Users\\shaul\\Documents\\GitHub\\academix-ydata-project\\code'
@@ -160,10 +163,9 @@ if __name__ == "__main__":
   df.rename(columns={'ORG_STATE':'last_author_country'},inplace=True)
 
   #Get unique authors
-  df = top_N_indie_authors(df,5)
+  df = top_N_indie_authors(df,12)
   df = get_similarity_matrix(ps,df)
   X_train, y_train, X_test, y_test = get_train_test(df,0.8)
-  y_hat = log_model(X_train,y_train,X_test,y_test)
-  plt.hist(y_hat,bins=50)
-  
+  score, y_hat = log_model(X_train,y_train,X_test,y_test)
+  print(score)
   
