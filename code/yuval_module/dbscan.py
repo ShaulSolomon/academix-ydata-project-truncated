d1ebab18f497d44e15a192314a50045adc38c7da
Roy Granit <roy@academix.bio>
2020-05-20 16:14:39 +0300

    Update dbscan.py

--- code/yuval_module/dbscan.py
+++ code/yuval_module/dbscan.py
@@ -8,14 +8,23 @@ import pandas as pd
 import numpy as np
 import utils as utils
 
-def run_db_scan(author_df: pd.DataFrame):
+def run_db_scan(author_df: pd.DataFrame, 
+                gammas:dict={
+                                "author":0.5,
+                                "mesh":0.3,
+                                "inst":0.1,
+                                "email":0.1,
+                                "country":0.0}, 
+                eps:float=1.27 ):
         """
         run DBscan using yuval's code
         params:
-                author_df(dataframe): a data frame of publications  
+                author_df(dataframe): a data frame of publications 
+                gammas(dict): dict of weight
+                eps(float): epsilon
         """
         print("Running Yuval's DBscan\n")
-        paper_clusterer=PaperClusterer(eps=1.27)
+        paper_clusterer=PaperClusterer(eps, gammas)
         # dist matrix
         combined_dist, combined_sim, total_df = paper_clusterer.get_dist_matrix(author_df)
         # cluster
