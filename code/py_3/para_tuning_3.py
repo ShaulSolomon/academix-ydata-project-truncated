c391c2e62956a6ea736f9a929beaa4f1ac0d9462
Ubuntu <ubuntu@ip-172-31-67-75.ec2.internal>
2020-07-06 10:32:08 +0000

    Trying here

--- code/py_3/para_tuning_3.py
+++ code/py_3/para_tuning_3.py
@@ -18,11 +18,10 @@ import itertools
 
 from sklearn.linear_model import LogisticRegression as LogR
 
-def pipeline(df, train_set,val_auth,ps):
+def pipeline(df, sim_matrix_train,dict_auth,ps):
     '''
     Pipelines from Author Names -> Pairs Weights -> Log R model -> DB Scan -> F1 Score
     '''
-    sim_matrix_train, scaler = sim_matrix_3.get_similarity_matrix(ps,train_set,scaler=None,flag_base=True)    
     X_train, y_train = lr_model_3.get_train_all(sim_matrix_train)
 
     ###Possible hyper-parameters to tune
@@ -38,24 +37,18 @@ def pipeline(df, train_set,val_auth,ps):
     best_solver = None
     best_eps = None
     best_F1 = 0.0
-    
-    dict_auth = {}
-    for auth in val_auth:
-        dict_auth[auth] = {}
-        dict_auth[auth]['df'] = df[df["last_author_name"] == auth]
-        dict_auth[auth]['sim_mat'],_ = sim_matrix_3.get_similarity_matrix(ps,dict_auth[auth]['df'],scaler,flag_base = False)
 
     for (lr_penalty, lr_C, lr_solver, db_eps) in all_combinations:
         try:
             log = LogR(penalty=lr_penalty, C=lr_C, solver=lr_solver)
             model = log.fit(X_train,y_train)
-            
             weights = model.coef_[0]
             bias = model.intercept_[0]
             
             df_all_cases = []
             
-            for auth in val_auth:
+            
+            for auth in dict_auth.keys():
                 df_auth = dict_auth[auth]['df']
                 df_sim =  dict_auth[auth]['sim_mat']
                 X_feat = df_sim.iloc[:,:-1]
@@ -63,7 +56,7 @@ def pipeline(df, train_set,val_auth,ps):
                 num_paper = int(np.sqrt(len(X_feat_weights)))
                 dist_mat = np.array(X_feat_weights).reshape(num_paper,-1)
                 df_all_cases.append([df_auth,dist_mat])
-
+    
             y_hat_comb = []
 
             for case in df_all_cases:
@@ -72,21 +65,21 @@ def pipeline(df, train_set,val_auth,ps):
                 df_clus = df_clus[["pmid","PI_IDS"]]
                 df_clus['cluster_pred'] = y_hat.labels_
                 y_hat_comb.append(df_clus)
-
+                   
             f1, precision, recall, df_eval = metric_eval_2.get_metrics_many(y_hat_comb)
-            
-            return f1
-            
+                        
             if f1 > best_F1:
                 best_F1 = f1
                 best_penalty = lr_penalty
                 best_C = lr_C
                 best_solver = lr_solver
-                best_eps = df_eps     
+                best_eps = db_eps
+                best_weights = weights
+                best_bias = bias
         except:
             continue
             
-    return best_F1, best_penalty, best_C, best_solver, best_eps
+    return best_F1, best_penalty, best_C, best_solver, best_eps, best_weights, best_bias
     
     
     
